4bc9443bf1342bcce6ad7447e361151ce150d911
Steve Wilson <steve.wilson1088@gmail.com>
2020-11-03 20:55:47 +0000

    [index] - Updated index to include acf loop

--- themes/mcg/index.php
+++ themes/mcg/index.php
@@ -1,9 +1,19 @@
 <?php
 
-get_header() ?>
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://wphierarchy.com/
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package propeller
+ */
 
-
-<section class="js-app"></section>
-<? // Silence is Golden ?>
-
-<?php get_footer() ?>
+get_header();
+locate_template('parts/layout.php', true);
+get_footer();
